42a494ced026150a1f003571cb3b79f0ce6ae91f
Fatima-Bint <fatimatib44@gmail.com>
2020-12-02 09:00:34 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+//import Loader from './components/Loader'
+//import BrowserRoute{Route, Switch} from 'react-router-dom'
+
+export const AppContext = React.createContext();
+
 
 function App() {
+  const [isloading, setIsLoading] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AppContext.Provider value={{isloading, setIsLoading}}>
+
+    <div className='App'>
+      {/* <Switch>
+        <Route path='/' exact= {true} component={Home}/>
+      </Switch> */}
+
     </div>
-  );
+
+
+    </AppContext.Provider>
+
+    
+    
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/loader.css'
+
+const Loader = () => {
+    return (
+        <div className="loader-container">
+        <div className="lds-roller"><div>
+            </div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default Loader
